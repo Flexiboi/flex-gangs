3d5945335e4b5854ceb10296d20ee9bdb0ff73d7
Flexiboi <45070800+Flexiboi@users.noreply.github.com>
2023-11-02 19:53:31 +0100

    Add files via upload

---
+++ gangzones.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 02, 2023 at 07:53 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `orptestserver`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gangzones`
+--
+
+CREATE TABLE `gangzones` (
+  `id` int(11) NOT NULL,
+  `zone` varchar(50) NOT NULL,
+  `gang` varchar(255) NOT NULL,
+  `flag` text NOT NULL,
+  `stash` int(1) DEFAULT 0,
+  `furnance` int(1) DEFAULT 0,
+  `wash` int(1) DEFAULT 0,
+  `guards` int(1) DEFAULT 0,
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gangzones`
+--
+ALTER TABLE `gangzones`
+  ADD UNIQUE KEY `gangzones` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
